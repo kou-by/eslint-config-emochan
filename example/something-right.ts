0bcac878b42d14f093bf2dd20d93c85dd1572d74
Takashi Fujita <tgfjt.mail@gmail.com>
2021-05-18 10:54:51 +0900

    fix based on airbnb settings

--- example/something-right.ts
+++ example/something-right.ts
@@ -1,5 +1,6 @@
 const x1f = /u001f/;
 
+// eslint-disable-next-line no-console
 console.log(x1f);
 
 export const myname = 'emochan';
@@ -10,9 +11,7 @@ export enum Foo {
   bar = barVal,
 }
 
-export const obj = {
-  myname: myname,
-};
+export const obj = { myname };
 
 export function createFuture(x: string): string {
   return `is ${x}`;
